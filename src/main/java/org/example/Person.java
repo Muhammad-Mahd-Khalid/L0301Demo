3b108583146affe4181121f581b6f4b2a3b1c902
Jonathan Calver <calver@cs.toronto.edu>
2026-09-14 12:15:32 -0400

    override toString

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -6,8 +6,15 @@ public class Person {
 
   public static void main(String[] args) {
     Person p = new Person();
+    p.name = "Alice Aardvark";
+    p.age = 1;
     System.out.println(p.name);
     System.out.println(p.age);
     System.out.println(p);
+  }
+
+  @Override
+  public String toString() {
+    return this.name + "(" + age + ")";
   }
 }
